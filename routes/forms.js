89f45704f2682767a183b89a53d430d224f08730
Symbiot01 <114978589+Symbiot01@users.noreply.github.com>
2023-06-26 18:55:33 +0530

    Update forms.js

--- routes/forms.js
+++ routes/forms.js
@@ -2,13 +2,15 @@ const router = require('express').Router();
 // Back-end code
 router.get('/login', async (req, res) => {
 	return res.renderFile('forms/login.njk');
+});
 	
 router.get('/register', async (req, res) => {
 	return res.renderFile('forms/register.njk');
+});
 	
 router.get('/logged-in', async (req, res) => {
 	return res.renderFile('forms/loggged-in.njk');
-	
+});	
 router.get('/logged-out', async (req, res) => {
 	return res.renderFile('forms/logged-out.njk');
 });
